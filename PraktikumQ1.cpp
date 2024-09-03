c6707d87d7fa321b7a2bcf1c787b5e2a2c83695e
Ricadamm <adamrizky742@gmail.com>
2024-09-03 14:56:37 +0700

    Update PraktikumQ1.cpp

--- PraktikumQ1.cpp
+++ PraktikumQ1.cpp
@@ -3,30 +3,30 @@ using namespace std;
 
 int main()
 {
-    cout<<"Payslip for Employee:"<<endl;
-    cout<<"....................."<<endl;
+    cout << "Payslip for Employee:" << endl;
+    cout << "....................." << endl;
 
 
     string Name;
-    cout<<"Name: ";
-    cin>>Name ;
+    cout << "Name: ";
+    cin >> Name ;
 
     int Gross;
-    cout<<"Gross Salary: " << "Rp";
-    cin>>Gross ;
+    cout << "Gross Salary: " << "Rp";
+    cin >> Gross ;
 
     int Tax = (0.2)*Gross;
-    cout<<fixed<<"Tax (20%):  "<<"Rp"<<Tax<<endl;
+    cout << fixed << "Tax (20%):  " << "Rp" << Tax << endl;
 
     int Installment;
-    cout<<"Installment: "<<"Rp";
-    cin>>Installment;
+    cout << "Installment: " << "Rp";
+    cin >> Installment;
 
     int Insurance;
-    cout<<"Insurance: "<<"Rp";
-    cin>>Insurance ;
+    cout << "Insurance: " << "Rp";
+    cin >> Insurance ;
 
     int Net = Gross - Tax - Installment - Insurance;
-    cout<< fixed<< "Net Salary: "<<"Rp "<< Net;
+    cout << fixed << "Net Salary: " << "Rp " << Net;
 
 }
